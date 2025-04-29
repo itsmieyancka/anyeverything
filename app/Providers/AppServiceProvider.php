971c797a8f13c58091e3ea83fa1cb4b481e0d06f
Mace Ndhlovu <mercysndlovu@gmail.com>
2025-04-30 00:04:07 +0200

    install filament and assign roles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema; //For Schema
-
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Vite;
+use Spatie\Permission\Middleware\RoleMiddleware;
+use Spatie\Permission\Middleware\PermissionMiddleware;
+use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,9 +23,14 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-
     public function boot(): void
-    {    Schema::defaultStringLength(191);
+    {
+        Schema::defaultStringLength(191);
         Vite::prefetch(concurrency: 3);
+
+        // Register Spatie middlewares globally
+        Route::aliasMiddleware('role', RoleMiddleware::class);
+        Route::aliasMiddleware('permission', PermissionMiddleware::class);
+        Route::aliasMiddleware('role_or_permission', RoleOrPermissionMiddleware::class);
     }
 }
--- resources/js/Components/App/navbar.tsx
+++ resources/js/Components/App/navbar.tsx
@@ -41,8 +41,7 @@ function Navbar() {
                 <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                   <div className="w-10 rounded-full">
                     <img
-                      alt="Tailwind CSS Navbar component"
-                      src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"/>
+                   src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"/>
                   </div>
                 </div>
                 <ul
